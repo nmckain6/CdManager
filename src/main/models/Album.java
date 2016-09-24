e0920286efcdb8ccf14a864346d98f6224d8b314
nmckain6 <nmckain6@gmail.com>
2016-09-23 23:59:05 -0500

    Initial commit

---
+++ src/main/models/Album.java
@@ -0,0 +1,19 @@
+
+public class Album {
+    private String albumTitle;
+    private short albumYear;
+    private Band band;
+    
+    /*
+     * Default constructor; Necessary for Hibernate
+     */
+    public Album(){
+        
+    }
+    
+    public Album(String albumTitle, short albumYear, Band band){
+        
+    }
+    
+    
+}
